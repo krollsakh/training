7b470fda3c45fb7486178c2441168819cd501c8e
krollsakh <62526928+krollsakh@users.noreply.github.com>
2020-03-26 22:29:24 +0700

    Update HelloWorld.java

--- src/Pattern/HelloWorld.java
+++ src/Pattern/HelloWorld.java
@@ -2,8 +2,6 @@ package Pattern;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Who I Am?");
-        System.out.println("Forget it!!!");
+        System.out.println("Hello World!");
     }
 }
